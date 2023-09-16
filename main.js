013104cdf4cda9df1956e9cd96e00d51bc1e6c32
Jonash Gania <jonashtiglao@gmail.com>
2023-09-16 14:08:39 +0800

    Fix form error using Constraint Validation API

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 const showInputErrors = () => {
+  const fname = document.getElementById('first-name');
+  const lname = document.getElementById('last-name');
   const email = document.getElementById('email');
   const phone = document.getElementById('phone');
   const pass = document.getElementById('password');
   const confirmPass = document.getElementById('confirm-password');
   const emailError = document.querySelector('.email-error');
+  const fnameError = document.querySelector('.fname-error');
+  const lnameError = document.querySelector('.lname-error');
   const phoneError = document.querySelector('.phone-error');
   const passError1 = document.querySelector('.password-error1');
   const passError2 = document.querySelector('.password-error2');
   const confirmPassError = document.querySelector('.confirm-pass-error');
+
+
+  fname.addEventListener('input', () => {
+    if(fname.validity.valid){
+      fnameError.textContent = '';
+    }
+  })
+
+  lname.addEventListener('input', () => {
+    if(lname.validity.valid){
+      lnameError.textContent = '';
+    }
+  })
 
   email.addEventListener('input', () => {
     if(!email.validity.valid){
@@ -53,50 +70,86 @@ const showInputErrors = () => {
   })
 }
 
+const formSubmit = () => {
+  const form = document.getElementById('form');
+  const fname = document.getElementById('first-name');
+  const lname = document.getElementById('last-name');
+  const email = document.getElementById('email');
+  const phone = document.getElementById('phone');
+  const pass = document.getElementById('password');
+  const confirmPass = document.getElementById('confirm-password');
+  const fnameError = document.querySelector('.fname-error');
+  const lnameError = document.querySelector('.lname-error');
+  const emailError = document.querySelector('.email-error');
+  const phoneError = document.querySelector('.phone-error');
+  const passError1 = document.querySelector('.password-error1');
+  const passError2 = document.querySelector('.password-error2');
+  const confirmPassError = document.querySelector('.confirm-pass-error');
 
+  form.addEventListener('submit', (event) => {
+    if(fname.value === ''){
+      fnameError.textContent = '* This is required';
+      event.preventDefault();
+    } else {
+      fnameError.textContent = '';
+    }
+
+    if (lname.value === '') {
+      lnameError.textContent = '* This is required';
+      event.preventDefault();
+    } else {
+      lnameError.textContent = '';
+    }
+
+    if(!email.validity.valid){
+      emailError.textContent = '* Please enter a valid email address';
+      event.preventDefault();
+    } else if (email.value === ''){
+      emailError.textContent = '* This is required';
+      event.preventDefault();
+    } else {
+      emailError.textContent = '';
+    }
+
+
+    if(phone.validity.patternMismatch){
+      phoneError.textContent = '* Please follow this format (+63 XXX XXX XXXX)';
+      event.preventDefault();
+    } else if (phone.value === ''){
+      phoneError.textContent = '* This is required';
+      event.preventDefault();
+    } else {
+      phoneError.textContent = '';
+    }
+
+    if(pass.validity.tooShort){
+      passError1.textContent = '* Password must contain at least 8 characters';
+      event.preventDefault();
+    } else if (pass.value === '') {
+      passError1.textContent = '* This is required';
+      event.preventDefault();
+    } else {
+      passError1.textContent = '';
+    }
+
+    if(pass.validity.patternMismatch){
+      passError2.textContent = '* Password must include (numbers, uppercase characters, lowercase characters)';
+      event.preventDefault();
+    } else {
+      passError2.textContent = '';
+    }
+
+
+    if(pass.value !== confirmPass.value){
+      confirmPassError.textContent = '* Password does not match';
+      confirmPass.style.border = '1px solid red';
+      event.preventDefault();
+    } else {
+      confirmPassError.textContent = '';
+      confirmPass.style.border = '1px solid #9c9c9c';
+    }
+  })
+}
+
+formSubmit();
 showInputErrors();
-
-// const form = document.getElementById("form");
-
-// form.addEventListener("submit", function(event) {
-//   event.preventDefault();
-
-
-//   const errorElements = document.querySelectorAll(".error");
-//   errorElements.forEach(function(element) {
-//     element.textContent = "";
-//   });
-
-//   const email = document.getElementById("email").value;
-//   const phone = document.getElementById("phone").value;
-//   const password = document.getElementById("password").value;
-//   const confirmPassword = document.getElementById("confirm-password").value;
-
-//   let hasError = false;
-
-//   if (!validateEmail(email)) {
-//     // const emailError = document.querySelector("#email + .error");
-//     // emailError.textContent = "*Provide a valid email address.";
-//     // hasError = true;
-//   }
-
-//   if (!validatePhoneNumber(phone)) {
-//     // const phoneError = document.querySelector("#phone + .error");
-//     // phoneError.textContent = "*Provide a valid phone number.";
-//     // hasError = true;
-//   }
-
-//   if (password !== confirmPassword) {
-//     // const passwordError = document.querySelector("#password + .error");
-//     // passwordError.textContent = "*Password does not match.";
-//     // hasError = true;
-//   }
-// });
-
-// function validateEmail(email) {
-//   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-// }
-
-// function validatePhoneNumber(phone) {
-//   return /^(0|[0-9])[0-9]{9,10}$/.test(phone);
-// }
